bfd299536dcf5758aea35de785c52f00abe578d5
Diego Andrades <die_94_go@hotmail.com>
2024-05-18 20:11:50 +0200

    Creo proyecto

---
+++ PracticaDesarrolloSeguroDiegoAndrades/PracticaDesarrolloSeguroDiegoAndrades/PracticaDesarrolloSeguroDiegoAndradesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticaDesarrolloSeguroDiegoAndradesApp.swift
+//  PracticaDesarrolloSeguroDiegoAndrades
+//
+//  Created by Macbook Pro on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticaDesarrolloSeguroDiegoAndradesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
