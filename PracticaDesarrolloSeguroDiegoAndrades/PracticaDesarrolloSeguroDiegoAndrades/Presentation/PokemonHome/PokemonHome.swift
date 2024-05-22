f7580a9cb2ca26c4ecaf59d384db883c15a386c7
Diego Andrades <die_94_go@hotmail.com>
2024-05-22 22:11:19 +0200

    Añado botón a pantalla de inicio para poder probar zap quitando datos y activando wifi

--- PracticaDesarrolloSeguroDiegoAndrades/PracticaDesarrolloSeguroDiegoAndrades/Presentation/PokemonHome/PokemonHome.swift
+++ PracticaDesarrolloSeguroDiegoAndrades/PracticaDesarrolloSeguroDiegoAndrades/Presentation/PokemonHome/PokemonHome.swift
@@ -11,6 +11,7 @@ struct PokemonHome: View {
      
     @EnvironmentObject var rootViewModel: RootViewModel
     @State var cargarInfoPokemon : Bool = false
+    @State private var isLoading: Bool = false
     
     var body: some View {
         ZStack {
@@ -22,17 +23,64 @@ struct PokemonHome: View {
                 .opacity(0.9)
             
             // Logo
+            Spacer()
+ 
+            Spacer()
             VStack {
                 Image(decorative: "Logo")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 300, height: 300)
                 Spacer()
-                
-                ProgressView() // Spinner de carga
-                         .progressViewStyle(CircularProgressViewStyle(tint: .blue))
-                         .scaleEffect(3.0)
+                if self.isLoading {
+                    ProgressView() // Spinner de carga
+                        .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+                        .scaleEffect(3.0)
+                }
                 Spacer()
+                VStack{
+                    Button(action: {
+                        Task {
+                           let datos =  await rootViewModel.onPokemon() { readError in
+                                DispatchQueue.main.async {
+                                    switch readError {
+                                    case .serverError:
+                                        print("Server error pop up")
+                                    case .unknownError:
+                                        print("Unknown error pop up")
+                                    case .none:
+                                        print("Chargue Data Pokemon")
+                                        self.cargarInfoPokemon = true
+                                        self.isLoading = true
+
+                                    }
+                                }
+                            }
+                            
+                            if self.cargarInfoPokemon{
+                                guard let data = datos.first?.results else {
+                                    print("Error dispatch data from Pokemon")
+                                    return
+                                }
+                                for dato in data {
+                                    rootViewModel.listInfoPokemon.append(await rootViewModel.onListPokemon(dataPoke: dato))
+                                }
+                                rootViewModel.status = .loaded
+                            }
+                        }
+                        
+                    }, label: {
+                        Text("Entrar")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 138, height: 40)
+                            .background(Color(uiColor: UIColor(red: 128/255, green: 0/255, blue: 70/255, alpha: 1.0))) // Color background
+                            .cornerRadius(8.0)
+                            .shadow(radius: 10.0, x: 20, y: 10)
+                    })
+                }
                 Text("Práctica Desarrollo Seguro")
                     .font(.title2)
                     .bold()
@@ -41,37 +89,6 @@ struct PokemonHome: View {
                     .font(.title2)
                     .bold()
                     .foregroundStyle(Color(.blue))
-            }
-            
-        
-        }
-        .onAppear{
-            Task {
-               let datos =  await rootViewModel.onPokemon() { readError in
-                    DispatchQueue.main.async {
-                        switch readError {
-                        case .serverError:
-                            print("Server error pop up")
-                        case .unknownError:
-                            print("Unknown error pop up")
-                        case .none:
-                            print("Chargue Data Pokemon")
-                            self.cargarInfoPokemon = true
-
-                        }
-                    }
-                }
-                
-                if self.cargarInfoPokemon{
-                    guard let data = datos.first?.results else {
-                        print("Error dispatch data from Pokemon")
-                        return
-                    }
-                    for dato in data {
-                        rootViewModel.listInfoPokemon.append(await rootViewModel.onListPokemon(dataPoke: dato))
-                    }
-                    rootViewModel.status = .loaded
-                }
             }
         }
     }
